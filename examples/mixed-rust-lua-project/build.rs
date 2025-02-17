0db06e1d35f55bb1d6cf2025bce7dd51ea7ad996
kkew3 <kps6326@hotmail.com>
2025-02-17 10:35:51 +0800

    Revert "Try debugging lua link error on windows"
    This reverts commit d432b20768e5a80d2ba2e8f8c9fd8e3428e4d064.

--- examples/mixed-rust-lua-project/build.rs
+++ examples/mixed-rust-lua-project/build.rs
@@ -1,7 +1,7 @@
 use std::path::PathBuf;
 
 fn main() {
-    // add_extension_module_link_args();
+    add_extension_module_link_args();
     let my_package_dir: PathBuf =
         [env!("CARGO_MANIFEST_DIR"), "lua"].iter().collect();
     expose_shared_library::expose_shared_library(my_package_dir).unwrap();
