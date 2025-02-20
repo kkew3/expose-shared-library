d0731ae1d43fc82ba7aaab5e2215c0af156cc2ba
kkew3 <kps6326@hotmail.com>
2025-02-20 11:10:16 +0800

    Improve the examples
    - Make the assertions explicit.
    - Flatten unnecessary directories in the python example.

--- examples/mixed-rust-lua-project/lua/main.lua
+++ examples/mixed-rust-lua-project/lua/main.lua
@@ -1,1 +1,2 @@
-print(require("sum_as_string_rs").sum_as_string(1, 2))
+local mod = require("sum_as_string_rs");
+assert(mod.sum_as_string(1, 2) == "3")
--- examples/mixed-rust-python-project/build.rs
+++ examples/mixed-rust-python-project/build.rs
@@ -3,8 +3,6 @@ use std::path::PathBuf;
 fn main() {
     pyo3_build_config::add_extension_module_link_args();
     let my_package_dir: PathBuf =
-        [env!("CARGO_MANIFEST_DIR"), "python/my_package"]
-            .iter()
-            .collect();
+        [env!("CARGO_MANIFEST_DIR"), "python"].iter().collect();
     expose_shared_library::expose_shared_library(my_package_dir).unwrap();
 }
